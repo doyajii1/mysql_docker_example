1d195bc5f38d061476fbc0412d4db8cb7f727149
hk ra <doyajii1@gmail.com>
2020-09-03 22:10:50 +0900

    build.sh script makes backup if the directory exists. This will prevents overriding data directory.

--- build.sh
+++ build.sh
@@ -1,3 +1,24 @@
+#backup data directory if directoy exist
+#checks if /mysql_data folder exists
+if [ -d "./mysql_data" ] 
+then
+    #creates backup directory
+    echo "Directory ./mysql_data exists. Creating backup..." 
+    if [[ ! -e "./backup" ]]; then
+        mkdir "./backup"
+    fi
+    date_dir_path=$(date +'%Y/%m/%d')
+    time=$(date +%H-%M-%S)
+    #prepares backup directory in Year/Month/Date/time/
+    #-p parameter creates directories if any of them are not found
+    mkdir -p ./backup/$date_dir_path/$time/
+    echo "Moved ./mysql_data to ./backup/$date_dir_path/$time" 
+    mv -f ./mysql_data ./backup/$date_dir_path/$time/
+else
+    echo "Directory ./mysql_data does not exists. Nothing to backup..."
+fi
+
+
 echo "attempt to stop any mysql container..."
 docker stop mysql
 
