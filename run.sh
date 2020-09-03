f4c8e3d3406a08d621b1a83733ac7a9eb1aeddbc
hk ra <doyajii1@gmail.com>
2020-09-03 16:17:20 +0900

    fixing volume path for mysql for run.sh file

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 echo "starting container..."
-docker run -it -d -p 3306:3306 -v $PWD/mysql_data/mysql:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=root --rm --name mysql mysql_docker:v0.1
+docker run -it -d -p 3306:3306 -v $PWD/mysql_data/:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=root --rm --name mysql mysql_docker:v0.1
 docker ps
 echo "started!"
