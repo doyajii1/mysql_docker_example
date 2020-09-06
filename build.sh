677203018bcb75834d039dcc37d08dd34c5059b6
hk ra <doyajii1@gmail.com>
2020-09-06 23:31:07 +0900

    build.sh db init run requires root pwd

--- build.sh
+++ build.sh
@@ -24,7 +24,7 @@ docker stop mysql
 
 echo "execute docker-compose build..."
 docker build --tag mysql_docker:v0.1 .
-docker run -it -d -p 3306:3306 --rm --name mysql mysql_docker:v0.1
+docker run -it -d -p 3306:3306 -e MYSQL_ROOT_PASSWORD=root --rm --name mysql mysql_docker:v0.1
 
 echo "copying database file to host..."
 docker cp mysql:/var/lib/mysql/ ./mysql_data/
